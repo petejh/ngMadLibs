acd8154919f39e6d21ce56a06d4cf84cb694bd4c
petejh <petejh@truly.fake>
2014-06-17 20:33:07 -0700

    Refactor to reduce code duplication
    Replace $scope.pronouns initialization with call to setPronouns(). Replace each word in the wordlist with a hash containing a value string and placeholder text. Add isFilled() to tell ng-class whether the user has supplied a unique value for the fillin field.

--- js/ngMadLibs.js
+++ js/ngMadLibs.js
@@ -1,15 +1,33 @@
 angular.module("ngMadLibs", [])
   .controller("ngMadLibsCtrl", function($scope) {
     var wordlist = {
-      name: "",
-      job_title: "",
-      tedious_task: "",
-      dirty_task: "",
-      celebrity: "",
-      useless_skill: "",
-      obnoxious_celebrity: "",
-      huge_number: "",
-      adjective: ""
+      name: {
+        value: "",
+        placeholder: "name" },
+      job_title: {
+        value: "",
+        placeholder: "job title" },
+      tedious_task: {
+        value: "",
+        placeholder: "tedious task" },
+      dirty_task: {
+        value: "",
+        placeholder: "dirty task" },
+      celebrity: {
+        value: "",
+        placeholder: "celebrity" },
+      useless_skill: {
+        value: "",
+        placeholder: "useless skill" },
+      obnoxious_celebrity: {
+        value: "",
+        placeholder: "obnoxious celebrity" },
+      huge_number: {
+        value: "",
+        placeholder: "huge number" },
+      adjective: {
+        value: "",
+        placeholder: "adjective" }
     };
 
     var pronouns = {
@@ -29,12 +47,18 @@ angular.module("ngMadLibs", [])
       }
     };
 
-    $scope.wordlist = wordlist;
-    $scope.pronouns = pronouns.female;
-    $scope.gender = "female";
-
     $scope.setPronouns = function() {
       $scope.pronouns = pronouns[$scope.gender];
-    }
+    };
+
+    $scope.isFilled = function(field) {
+      return( field.value && (field.value != field.placeholder));
+    };
+
+    // Initializations
+    $scope.wordlist = wordlist;
+
+    $scope.gender = "female";
+    $scope.setPronouns();
   });
 
